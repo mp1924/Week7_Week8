ab49e465c49343f53fe801caccd1d9b73f02e1e1
mp1924 <mp1924@srmist.edu.in>
2026-04-06 09:29:57 +0530

    Create Program3.java

---
+++ Level1_Programs/Program3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TriangleRun {
+
+    public static double calculateRounds(double a, double b, double c, double totalDistance) {
+        double perimeter = a + b + c;
+        return totalDistance / perimeter;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double rounds = calculateRounds(a, b, c, 5000);
+
+        System.out.println("Rounds = " + rounds);
+
+        sc.close();
+    }
+}
